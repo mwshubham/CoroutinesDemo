e4f340a8756e783e6e3e3892ee52063522466862
Shubham Agarwal <shubham.agarwal@railyatri.in>
2019-08-23 17:53:06 +0530

    updated global logger utils

--- app/src/main/java/com/example/coroutinesdemo/GlobalLoggerUtils.kt
+++ app/src/main/java/com/example/coroutinesdemo/GlobalLoggerUtils.kt
@@ -1,8 +1,6 @@
 package com.example.coroutinesdemo
 
-import android.content.Context
 import android.util.Log
-import android.widget.Toast
 
 /**
  * This Class is just to display log messages
@@ -28,71 +26,5 @@ object GlobalLoggerUtils {
             e.printStackTrace()
         }
     }
-
-    @JvmStatic
-    fun logError(TAG: String?, message: String?) {
-        if (toShowLog && TAG != null && message != null) {
-            Log.e(TAG, message + " " + "[Thread: " + Thread.currentThread().name + "]")
-        }
-    }
-
-    @JvmStatic
-    fun largeLog(tag: String, message: String?) {
-        if (toShowLog && message != null) {
-            try {
-                if (message.length > 4000) {
-                    showLog(tag, message.subSequence(0, 4000).toString())
-                    largeLog(tag, message.subSequence(4000, message.length - 1).toString())
-                } else {
-                    showLog(tag, message)
-                }
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
-    }
-
-    @JvmStatic
-    fun showToast(context: Context, MSG: String?) {
-        if (toShowLog && MSG != null) {
-            Toast.makeText(context, MSG, Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    //    public static void showNotification(Context context, String MSG) {
-    //        if (MSG != null && toShowLog) {
-    //            String title = "Debug Notification";
-    //
-    // Use generic approach via application to get the class , no need to hardcode here...
-
-    //            Intent i = new Intent(context, HomePageActivity.class);
-    //            Bundle bundle = new Bundle();
-    //            i.putExtras(bundle);
-    //            PendingIntent pending = PendingIntent.getActivity(
-    //                    context.getApplicationContext(), 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
-    //
-    //            NotificationManager NM = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-    //            NotificationCompat.Builder builder =
-    //                    new NotificationCompat.Builder(context, PRIMARY_NOTIF_CHANNEL);
-    //            builder.setContentTitle(title);
-    //            builder.setContentText(MSG);
-    //            builder.setSmallIcon(CommonUtility.getNotificationIcon());
-    //            builder.setStyle(new NotificationCompat.BigTextStyle().bigText(MSG));
-    //            builder.setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher));
-    //            builder.setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE | Notification.DEFAULT_LIGHTS);
-    //            builder.setContentIntent(pending);
-    //            builder.build();
-    //            builder.setChannelId("default");
-    //            Notification notify = builder.getNotification();
-    //            notify.sound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-    //
-    //            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-    //                NM.createNotificationChannel(CommonUtility.oreoNotification(context));
-    //            }
-    //            if (NM != null) {
-    //                NM.notify(1, notify);
-    //            }
-    //        }
-    //    }
 
 }
